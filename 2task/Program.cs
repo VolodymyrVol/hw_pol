784f2b15d93b0c10fbec0286c2bf6deec8940fa5
Vova <lololoskafry@gmail.com>
2024-11-27 00:23:01 +0200

    Add project files.

---
+++ 2task/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _2task
+{
+    internal class Program
+    {
+        class MusIns
+        {
+            #region Areas
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string History { get; set; }
+            #endregion
+            #region constr  
+            public MusIns()
+            {
+                Name = null;
+                Description = null;
+            }
+            public MusIns(string name, string description)
+            {
+                Name = name;
+                Description = description;
+            }
+            #endregion
+            #region Meth
+            public void Sound() { Console.WriteLine("Base Sound"); }
+            public void Show() { Console.WriteLine("Base Show"); }
+            public void Desc() { Console.WriteLine("Base Desc"); }
+            public void Hist() { Console.WriteLine("Base hist"); }
+
+            #endregion
+        }
+        class Violin : MusIns
+        {
+            public new void Sound() { Console.WriteLine("Violin sound"); }
+            public new void Show() { Console.WriteLine($"Violin {Name}"); }
+            public new void Desc() { Console.WriteLine($"Violin {Description}"); }
+            public void Hist() { Console.WriteLine($"Violin {History}"); }
+        }
+        class Trombone : MusIns
+        {
+            public new void Sound() { Console.WriteLine($"Trombone sound"); }
+            public new void Show() { Console.WriteLine($"Trombone {Name}"); }
+            public new void Desc() { Console.WriteLine($"Trombone {Description}"); }
+            public void Hist() { Console.WriteLine($"Trombone {History}"); }
+        }
+        class Ukulele : MusIns
+        {
+            public new void Sound() { Console.WriteLine("Ukulele sound"); }
+            public new void Show() { Console.WriteLine($"Ukulele {Name}"); }    
+            public new void Desc() { Console.WriteLine($"Ukulele {Description}"); }
+            public void Hist() { Console.WriteLine($"Ukulele {History}"); }
+        }
+        class Cello : MusIns
+        {
+            public new void Sound() { Console.WriteLine("Cello sound"); }
+            public new void Show() { Console.WriteLine($"Cello {Name}"); }
+            public new void Desc() { Console.WriteLine($"Cello {Description}"); }
+            public void Hist() { Console.WriteLine($"Cello {History}"); }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ hw_pol/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace hw_pol
+{
+    class Device
+    {
+        #region Areas
+        public string Name { get; set; }
+        public string Description { get; set; }
+        #endregion
+        #region constr  
+        public Device()
+        {
+            Name = null;
+            Description = null;
+        }
+        public Device(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+        #endregion
+        #region Meth
+        public void Sound() { Console.WriteLine("Base Sound"); }
+        public void Show() { Console.WriteLine("Base Show"); }
+        public void Desc() { Console.WriteLine("Base Desc"); }
+
+        #endregion
+    }
+    class Teapot : Device
+    {
+        public new void Sound() { Console.WriteLine("Teapot sound"); }
+        public new void Show() { Console.WriteLine($"Teapot {Name}"); }
+        public new void Desc() { Console.WriteLine($"Teapot {Description}"); }
+    }
+    class Auto : Device
+    {
+        public new void Sound() { Console.WriteLine($"Auto sound"); }
+        public new void Show() { Console.WriteLine($"Auto {Name}"); }
+        public new void Desc() { Console.WriteLine($"Auto {Description}"); }
+    }
+    class Microv : Device
+    {
+        public new void Sound() { Console.WriteLine("Microv sound"); }
+        public new void Show() { Console.WriteLine($"Microv {Name}"); }
+        public new void Desc() { Console.WriteLine($"Microv {Description}"); }
+    }
+    class Steamship : Device
+    {
+        public new void Sound() { Console.WriteLine("Steamship sound"); }
+        public new void Show() { Console.WriteLine($"Steamship {Name}"); }
+        public new void Desc() { Console.WriteLine($"Steamship {Description}"); }
+    }
+
+    internal class Program
+        {
+        }   
+  }
